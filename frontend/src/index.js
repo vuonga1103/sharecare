d878183644ef55f905bf4fcb193489f3be319f42
“DonatelloTHM” <donat.pllana@outlook.com>
2020-08-14 19:16:22 -0400

    signup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,52 @@
 const registerBtn = document.querySelector("#register-btn"),
       loginDiv = document.querySelector("div#login"),
       registerDiv = document.querySelector("div#register-div"),
-      registerForm = document.querySelector("form#register-form")
+      registerForm = document.querySelector("form#register-form"),
+     signUpButton = document.getElementById("signUp"),
+     signInButton = document.getElementById("signIn"),
+     container = document.getElementById("container");
 
-registerBtn.addEventListener("click", displayRegisterDiv);
+ signUpButton.addEventListener("click",(evt) => {
+   container.classList.add("right-panel-active");
+ })
+ signInButton.addEventListener("click",(evt) => {
+   container.classList.remove("right-panel-active");
+ })
 
-function displayRegisterDiv(evt) {
-  loginDiv.hidden = true;
-  registerDiv.hidden = false;
-}
 
-registerForm.addEventListener("submit", createPrimaryCaregiver);
-registerForm
-function createPrimaryCaregiver(evt) {
-  evt.preventDefault();
 
-  const usernameInput = evt.target['register-username'].value,
-        emailInput = evt.target['register-email'].value,
-        nameInput = evt.target['register-name'].value,
-        roleInput = evt.target['register-role'].value;
 
-  const newPrimaryCaregiver = {
-    name: nameInput,
-    username: usernameInput,
-    email: emailInput,
-  }
 
-  renderNewCareReceiverForm(newPrimaryCaregiver);
-}
 
-function renderNewCareReceiverForm(newPrimaryCaregiver) {
-  // display form
 
-  // add event listener on form, passing in newPrimaryCaregiver, pass that along into the newCareReceiver function
-}
+// registerBtn.addEventListener("click", displayRegisterDiv);
+
+// function displayRegisterDiv(evt) {
+//   loginDiv.hidden = true;
+//   registerDiv.hidden = false;
+// }
+
+// registerForm.addEventListener("submit", createPrimaryCaregiver);
+// registerForm
+// function createPrimaryCaregiver(evt) {
+//   evt.preventDefault();
+
+//   const usernameInput = evt.target['register-username'].value,
+//         emailInput = evt.target['register-email'].value,
+//         nameInput = evt.target['register-name'].value,
+//         roleInput = evt.target['register-role'].value;
+
+//   const newPrimaryCaregiver = {
+//     name: nameInput,
+//     username: usernameInput,
+//     email: emailInput,
+//   }
+
+//   renderNewCareReceiverForm(newPrimaryCaregiver);
+// }
+
+// function renderNewCareReceiverForm(newPrimaryCaregiver) {
+//   // display form
+
+//   // add event listener on form, passing in newPrimaryCaregiver, pass that along into the newCareReceiver function
+// }
