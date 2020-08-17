8d453817f8fae82f15f6cc8ed304ebb8e2161922
“DonatelloTHM” <donat.pllana@outlook.com>
2020-08-16 23:31:01 -0400

    few changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 // STABLE ELEMENTS
+
 const registerBtn = document.querySelector("#register-btn"),
   loginDiv = document.querySelector("div#login"),
+  getHtmlTag = document.querySelector("html")
   registerDiv = document.querySelector("div#register-div"),
   registerForm = document.querySelector("form#register-form"),
   signUpButton = document.getElementById("signUp"),
@@ -23,6 +25,38 @@ const registerBtn = document.querySelector("#register-btn"),
 let loggedInCaregiver;
 
 dashboard.style.display = "none";
+
+new Sortable(dashboard, {
+  animation: 150,
+});
+
+var elem = postsUl
+var infScroll = new InfiniteScroll( elem, {
+  // options
+  history: false,
+});
+
+Sortable.create(postsUl, {
+  group: {
+  name: "posts-ul",
+  pull: true
+  },
+  animation: 100,
+  // onEnd: function (evt){
+  //   debugger
+  // }
+});
+
+Sortable.create(importantPostsUl,{
+  group: {
+    name: "priority-posts",
+    put: ["posts-ul"]
+    },
+  animation: 100,
+  onEnd: function (evt){
+    debugger
+  }
+});
 
 
   
@@ -166,6 +200,7 @@ function displayDashboard(caregiver) {
 function displayPosts() {
   firstShowScreen.hidden = true;
 
+  getHtmlTag.style = "background: url('https://cdn.aarp.net/content/dam/aarp/work/Work_at_50%2B/2018/11/1140-caregiving-jobs.jpg') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; transition: 1s; box-shadow: inset 0 0 0 1000px rgba(0,0,0,.3);"
   const care_receiver_id = loggedInCaregiver.care_receiver_id;
 
   fetch(`http://localhost:3000/care-receivers/${care_receiver_id}/posts`)
